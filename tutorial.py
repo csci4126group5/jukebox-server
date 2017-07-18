c1a5404ceee5699bf212ae03233ddafee5d7f2ec
salockhart <salexlockhart@gmail.com>
2017-07-18 16:52:17 -0300

    comments on tutorial

--- tutorial.py
+++ tutorial.py
@@ -4,12 +4,16 @@ Flask API Tutorial
 By Alex Lockhart and Asma Alhajri
 """
 
+# Step 1
 from flask import Flask, jsonify, request
 
+# Step 1
 APP = Flask(__name__)
 
+# Step 1
 GROUPS = ["Best Group", "Workout Team"]
 
+# Step 1
 @APP.route('/', methods=['GET'])
 def get_groups():
     """
@@ -17,6 +21,7 @@ def get_groups():
     """
     return jsonify(GROUPS)
 
+# Step 2
 @APP.route('/', methods=['POST'])
 def add_group():
     """
@@ -27,6 +32,7 @@ def add_group():
     GROUPS.append(body)
     return jsonify(GROUPS)
 
+# Step 3
 @APP.route('/<group_number>', methods=['GET'])
 def get_group(group_number):
     """
@@ -35,6 +41,7 @@ def get_group(group_number):
     index = int(group_number)
     return jsonify(GROUPS[index])
 
+# Step 4
 @APP.route('/<group_number>', methods=['PUT'])
 def update_group(group_number):
     """
@@ -45,6 +52,7 @@ def update_group(group_number):
     GROUPS[index] = body
     return jsonify(GROUPS[index])
 
+# Step 4
 @APP.route('/<group_number>', methods=['DELETE'])
 def delete_group(group_number):
     """
@@ -54,5 +62,6 @@ def delete_group(group_number):
     GROUPS.pop(index)
     return "OK"
 
+# Step 1
 if __name__ == '__main__':
     APP.run(debug=True)
